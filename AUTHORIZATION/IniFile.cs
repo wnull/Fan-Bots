51d04080b62d0f6c45ec785a0a927ca671d8d98f
Daniil Dilako <github_lako@mail.ru>
2020-08-15 21:22:00 +0500

    Update IniFile.cs
    Signed-off-by: Daniil Dilako <github_lako@mail.ru>

--- AUTHORIZATION/IniFile.cs
+++ AUTHORIZATION/IniFile.cs
@@ -4,17 +4,15 @@ using System.Text;
 
 namespace AUTHORIZATION
 {
-    internal class IniFile
+    public class IniFile
     {
         private readonly string Path;
-
-        [DllImport("kernel32", CharSet = CharSet.Ansi)]
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
         private static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
 
-        [DllImport("kernel32", CharSet = CharSet.Ansi)]
         private static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
-
-        public IniFile(string IniPath = null) => Path = new FileInfo(IniPath + ".ini").FullName.ToString();
+        public IniFile(string IniPath = null) => Path = new FileInfo(IniPath).FullName.ToString();
 
         public string Read(string Key, string Section = null)
         {
@@ -23,11 +21,8 @@ namespace AUTHORIZATION
             return RetVal.ToString();
         }
         public void Write(string Key, string Value, string Section = null) => WritePrivateProfileString(Section, Key, Value, Path);
-
         public void DeleteKey(string Key, string Section = null) => Write(Key, null, Section);
-
         public void DeleteSection(string Section = null) => Write(null, null, Section);
-
         public bool KeyExists(string Key, string Section = null) => Read(Key, Section).Length > 0;
     }
 }
